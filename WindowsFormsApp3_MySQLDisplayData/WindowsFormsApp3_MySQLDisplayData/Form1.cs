d29632fca6bb929f01e2d82c5cf17d762c0cf200
Dino Emso <48333019+dinoemso97@users.noreply.github.com>
2021-05-04 14:16:29 +0200

    Add files via upload

---
+++ WindowsFormsApp3_MySQLDisplayData/WindowsFormsApp3_MySQLDisplayData/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp3_MySQLDisplayData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+
+                MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM database2.users", connection);
+
+                connection.Open();
+
+                DataSet ds = new DataSet();
+                adapter.Fill(ds,"users");
+                dataGridView1.DataSource = ds.Tables["users"];
+                connection.Close();
+
+            }
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp4_ComboBoxMySQL/WindowsFormsApp4_ComboBoxMySQL/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp4_ComboBoxMySQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+
+                MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+                string selectQuery = "SELECT * FROM database2.users";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(selectQuery, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) {
+
+                    comboBox1.Items.Add(reader.GetString("fname"));
+                
+                
+                }
+            
+            }
+            catch(Exception ex) {
+
+                MessageBox.Show(ex.Message);
+            
+            }
+        }
+    }
+}
