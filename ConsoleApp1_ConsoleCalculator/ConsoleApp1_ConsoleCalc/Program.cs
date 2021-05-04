21a3f6c611a4183d1282d09d5effbad869a5d2c8
Dino Emso <48333019+dinoemso97@users.noreply.github.com>
2021-05-04 13:49:55 +0200

    Add files via upload

---
+++ ConsoleApp1_ConsoleCalculator/ConsoleApp1_ConsoleCalc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1_ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine(); 
+
+
+            Console.Write("Enter a second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (op == "+") {
+
+                Console.Write(num1 + num2);
+
+            }
+            else if (op == "-") {
+
+                Console.Write(num1 - num2);
+            
+            
+            }
+            else if (op == "/")
+            {
+
+                Console.Write(num1 / num2);
+
+
+            }
+            else if (op == "*")
+            {
+
+                Console.Write(num1 * num2);
+
+
+            }
+            else {
+
+                Console.WriteLine("Invalid Operator!");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
